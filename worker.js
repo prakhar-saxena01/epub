fa59db273ba422e541df400d79b57546fb9f6544
Andrew Dolgov <noreply@fakecake.org>
2017-02-26 17:28:25 +0300

    worker: get cached files on install

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 
 var CACHE_NAME = 'epube-v1';
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
 		var urls = [
