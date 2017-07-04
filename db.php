00ff14efa794f2ada3a8dbe51fc4ef2951cfe4da
Andrew Dolgov <noreply@fakecake.org>
2017-07-04 13:40:36 +0300

    sqlite: enable WAL

--- db.php
+++ db.php
@@ -6,6 +6,7 @@ class Db {
 	private function __construct() {
 		$this->dbh = new SQLite3(__DIR__ . "/" . SCRATCH_DB);
 		$this->dbh->busyTimeout(30*1000);
+		$this->dbh->exec('PRAGMA journal_mode = wal;');
 	}
 
 	public static function get() {
