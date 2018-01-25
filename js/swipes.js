c68135b5a4a30d379b8f923f29ed45ee37e47f49
Andrew Dolgov <noreply@fakecake.org>
2018-01-25 19:57:08 +0300

    toggle UI/fs on middle tap

--- js/swipes.js
+++ js/swipes.js
@@ -1,6 +1,11 @@
 $(window).on("click tap", function() {
-	parent.show_ui(true);
-	parent.disable_fullscreen();
+	if (parent.$(".header").is(":visible")) {
+		parent.show_ui(false);
+		parent.request_fullscreen();
+	} else {
+		parent.show_ui(true);
+		parent.disable_fullscreen();
+	}
 });
 
 $(window).on("swipeleft", function() {
