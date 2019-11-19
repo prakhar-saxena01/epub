e71b809160d6e7a85c1e7d0a0c7ff5db57dbca4f
Andrew Dolgov <noreply@fakecake.org>
2019-11-19 15:05:07 +0300

    disable skipWaiting on install (sometimes breaks initial reload)

--- worker.js
+++ worker.js
@@ -40,10 +40,6 @@ const CACHE_URLS = [
 			'lib/fonts/pmn-caecilia-75.ttf'
 		];
 
-self.addEventListener('install', function(event) {
-	self.skipWaiting();
-});
-
 self.addEventListener('activate', function(event) {
 	event.waitUntil(
 		caches.keys().then(function(keyList) {
